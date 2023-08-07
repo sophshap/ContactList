0d88443d4f21d425ea7e1285a4913c3a03fb281a
sophshap <sophieashapiro@gmail.com>
2023-08-07 12:17:51 -0400

    rendered table

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
 
   return (
     <>
-      <div>
-
-      </div>
-
-
+      <ContactList />
     </>
-  )
+  );
 }
-
-export default App
