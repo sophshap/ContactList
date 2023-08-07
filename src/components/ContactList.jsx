bbe127af8bd94578f453829eaeff3f921869d121
sophshap <sophieashapiro@gmail.com>
2023-08-07 12:47:04 -0400

    table setup

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,17 +1,14 @@
-import React from "react";
-import {useState} from 'react';
+import { useState } from "react";
+import ContactRow from "./ContactRow";
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
-
-
-
 export default function ContactList() {
-    const [contacts, setContacts] = useState(dummyContacts)
-    console.log("Contacts: ", contacts)
+    const [contacts, setContacts] = useState(dummyContacts);
+
     return (
         <table>
             <thead>
@@ -25,9 +22,9 @@ export default function ContactList() {
                     <td>Email</td>
                     <td>Phone</td>
                 </tr>
-                {
-                    // Map over data here
-                }
+                {contacts.map((contact) => {
+                    return <ContactRow key={contact.id} contact={contact} />;
+                })}
             </tbody>
         </table>
     );
