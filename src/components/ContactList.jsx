cdd1f735021e391c5dea654e34dbc7e61f934772
winniemei <wmei91@gmail.com>
2023-08-07 13:03:14 -0400

    fetched data from api

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
-import ContactRow from "./ContactRow";
+import React from "react";
+import { useState, useEffect } from 'react';
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
-export default function ContactList() {
-    const [contacts, setContacts] = useState(dummyContacts);
 
+export default function ContactList() {
+    const [contacts, setContacts] = useState(dummyContacts)
+    useEffect(() => {
+        async function fetchContacts() {
+            try {
+                const response = await fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/todos/1');
+                const result = await response.json();
+                setContacts(result);
+            } catch(err) {
+                console.error(err);
+            }
+        }
+        console.log(contacts);
+        fetchContacts();
+    }, [])
+    console.log("Contacts: ", contacts)
     return (
         <table>
             <thead>
@@ -22,9 +36,9 @@ export default function ContactList() {
                     <td>Email</td>
                     <td>Phone</td>
                 </tr>
-                {contacts.map((contact) => {
-                    return <ContactRow key={contact.id} contact={contact} />;
-                })}
+                {
+                    // Map over data here
+                }
             </tbody>
         </table>
     );
