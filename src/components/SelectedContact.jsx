85b41348db468faa26830394707d4fd9fe9fb190
winniemei <wmei91@gmail.com>
2023-08-08 16:10:48 -0400

    added button to go back to all contacts

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
+import ContactList from './ContactList'
 
 export default function SelectedContact({selectedContactId, setSelectedContactId}) {
     const [contact, setContact] = useState({});
     useEffect(() => {
         async function fetchingFunction() {
             try {
+                if ({setSelectedContactId}) {
                 const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`)
                 const result = await response.json()
                 console.log(result);
                 setContact(result);
+            } else {
+                ContactList();
+            }
             } catch (error) {
                 console.error(error);
             }
@@ -19,6 +24,7 @@ export default function SelectedContact({selectedContactId, setSelectedContactId
         <div>
         <p>{contact.name}</p>
         <p>{contact.email}</p>
+        <button onClick = {() => {setSelectedContactId(null)}}>Back to List</button>
         </div>
     )
 }
