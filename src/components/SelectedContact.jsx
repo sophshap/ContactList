9780d55845a7e493d9e42aaa329db347e08940d6
winniemei <wmei91@gmail.com>
2023-08-08 16:31:20 -0400

    add address

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from 'react';
 import ContactList from './ContactList'
 
-export default function SelectedContact({selectedContactId, setSelectedContactId}) {
+export default function SelectedContact({ selectedContactId, setSelectedContactId }) {
     const [contact, setContact] = useState({});
     useEffect(() => {
         async function fetchingFunction() {
             try {
-                if ({setSelectedContactId}) {
-                const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`)
-                const result = await response.json()
-                console.log(result);
-                setContact(result);
-            } else {
-                ContactList();
-            }
+                if ({ setSelectedContactId }) {
+                    const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`)
+                    const result = await response.json()
+                    console.log(result);
+                    setContact(result);
+                } else {
+                    ContactList();
+                }
             } catch (error) {
                 console.error(error);
             }
@@ -22,9 +22,13 @@ export default function SelectedContact({selectedContactId, setSelectedContactId
     }, [])
     return (
         <div>
-        <p>Name: {contact.name}</p>
-        <p>Email: {contact.email}</p>
-        <button onClick = {() => {setSelectedContactId(null)}}>Back to List</button>
+            <p>Name: {contact.name}</p>
+            <p>Email: {contact.email}</p>
+            <p>Address:</p>
+            <p>{contact.address?.street}</p>
+            <p>{contact.address?.city}</p>
+            <p>{contact.address?.zipcode}</p>
+            <button onClick={() => { setSelectedContactId(null) }}>Back to List</button>
         </div>
     )
 }
